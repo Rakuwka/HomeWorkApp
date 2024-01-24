2134c41c760341262e2fad9be42ed999a185415e
Rakshanda <sshell450@gmail.com>
2024-01-24 20:31:04 +0400

    Initial Commit

---
+++ HomeWorkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkApp
+//
+//  Created by Rakshanda Alaskarova on 24.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
